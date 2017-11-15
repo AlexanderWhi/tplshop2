96b5432246d27da4c92bbf18b6780f05cb3bd41a
Alexander <Alexander@Alexander-PC>
2017-11-15 09:28:36 +0500

    Доработка ADV

--- core/component/adv/adv.tpl.php
+++ core/component/adv/adv.tpl.php
@@ -1,46 +1,46 @@
 <div>
-<form method="POST">
+    <form method="POST">
 
-<?$this->displayPageControl();?>
-<?if($rs->getCount()){?>
-<table class="grid">
-	<tr>
-		<th></th>
-		<th>Описание</th>
-		<th>Дата начала</th>
-		<th>Дата конца</th>
-		<th>Место</th>
-		<th>Показов</th>
-		<th>Кликов</th>
-		<th>Действие</th>
-	</tr>
-	<? while($rs->next()){?>
-		<tr>
-			<td style="width:200px">
-			<?if(isImg($rs->get('file'))){?>
-			<img src="<?=scaleImg($rs->get('file'),'w200h100')?>">
-			<?}?>
-			
-			</td>
-			<td style="width:200px">
-				 <a href="?act=edit&id=<?=$rs->get('id');?>"><small>[<?=htmlspecialchars($rs->get("description"));?>]</small></a>
-			</td>
-			<td><?=$rs->get("start_date");?></td>
-			<td><?=$rs->get("stop_date");?></td>
-			<td>[<?=$rs->get("place");?>] <?=$rs->get("pl_description");?></td>
-			<td><?=$rs->get("show_ad");?></td>
-			<td><?=$rs->get("click");?></td>
-			<td style="width:100px">
-				<a class="scrooll" href="?act=edit&id=<?=$rs->get('id');?>"><img src="/img/pic/edit_16.gif" title="Редактировать" alt="Редактировать"/></a>
-				<a class="scrooll " onclick="return confirm('Сброс?')" href="?act=reset&id=<?=$rs->get('id')?>"><img src="/img/pic/undo_16.gif" title="Сброс" alt="Сброс"/></a>
-				<a class="scrooll" onclick="return confirm('Вы действительно хотите удалить запись?')" href="?act=remove&id=<?=$rs->get('id');?>"><img src="/img/pic/trash_16.gif" title="Удалить запись" border="0" alt=""/></a>
-			</td>
-		</tr>	
-	<?}?>
-</table>
-<?$this->displayPageControl();?>
-<?}else{?>
-<div>Список пуст</div>
-<?}?>
-</form>
+        <? $this->displayPageControl(); ?>
+        <? if ($rs->getCount()) { ?>
+            <table class="grid">
+                <tr>
+                    <th></th>
+                    <th>Описание</th>
+                    <th>Дата начала</th>
+                    <th>Дата конца</th>
+                    <th>Место</th>
+                    <th>Показов</th>
+                    <th>Кликов</th>
+                    <th>Действие</th>
+                </tr>
+                <? while ($rs->next()) { ?>
+                    <tr>
+                        <td style="width:200px">
+                            <? if (isImg($rs->get('file'))) { ?>
+                                <img src="<?= scaleImg($rs->get('file'), 'w200h100') ?>">
+                            <? } ?>
+
+                        </td>
+                        <td style="width:200px">
+                            <a href="?act=edit&id=<?= $rs->get('id'); ?>"><small>[<?= my_htmlspecialchars($rs->get("description")); ?>]</small></a>
+                        </td>
+                        <td><?= $rs->get("start_date"); ?></td>
+                        <td><?= $rs->get("stop_date"); ?></td>
+                        <td>[<?= $rs->get("place"); ?>] <?= $rs->get("pl_description"); ?></td>
+                        <td><?= $rs->get("show_ad"); ?></td>
+                        <td><?= $rs->get("click"); ?></td>
+                        <td style="width:100px">
+                            <a class="scrooll" href="?act=edit&id=<?= $rs->get('id'); ?>"><img src="/img/pic/edit_16.gif" title="Редактировать" alt="Редактировать"/></a>
+                            <a class="scrooll " onclick="return confirm('Сброс?')" href="?act=reset&id=<?= $rs->get('id') ?>"><img src="/img/pic/undo_16.gif" title="Сброс" alt="Сброс"/></a>
+                            <a class="scrooll" onclick="return confirm('Вы действительно хотите удалить запись?')" href="?act=remove&id=<?= $rs->get('id'); ?>"><img src="/img/pic/trash_16.gif" title="Удалить запись" border="0" alt=""/></a>
+                        </td>
+                    </tr>	
+                <? } ?>
+            </table>
+            <? $this->displayPageControl(); ?>
+        <? } else { ?>
+            <div>Список пуст</div>
+        <? } ?>
+    </form>
 </div>
--- core/component/adv/adv_place.tpl.php
+++ core/component/adv/adv_place.tpl.php
@@ -1,38 +1,38 @@
 <div>
-<form method="POST">
-<button class="button" name="apply">Применить</button>
-<?$this->displayPageControl('plEdit');?>
-<?if($rs->getCount()){?>
-<table class="grid">
-	<tr>
-		<th>Идентификатор</th>
-		<th>Описание</th>
-		<th>Ширина</th>
-		<th>Высота</th>
-		<th></th>
-	</tr>
-	<? while($rs->next()){?>
-		<tr>
-			<td style="width:200px">
-				<img src="/img/pic/opnbr_16.gif" title="<?=htmlspecialchars($rs->get("description"));?>" alt="<?=htmlspecialchars($rs->get("description"));?>"/> 
-				<a href="?act=plEdit&id=<?=$rs->get('id');?>"><strong>[<?=$rs->get("id");?>]</strong></a>
-			</td>
-			<td>
-				<small>[<?=htmlspecialchars($rs->get("description"));?>]</small>
-			</td>
-			<td><input class="num" name="width[<?=$rs->get('id');?>]" value="<?=$rs->get('width')?>"></td>
-			<td><input class="num" name="height[<?=$rs->get('id');?>]" value="<?=$rs->get('height')?>"></td>
-			<td style="width:100px">
-				<a href="?act=plEdit&id=<?=$rs->get('id');?>"><img src="/img/pic/edit_16.gif" title="Редактировать" alt="Редактировать"/></a>
-				<a onclick="return confirm('Вы действительно хотите удалить запись?')" href="?act=plRemove&id=<?=$rs->get('id');?>"><img src="/img/pic/trash_16.gif" title="Удалить запись" border="0" alt=""/></a>
-			</td>
-		</tr>	
-	<?}?>
-</table>
-<?$this->displayPageControl('plEdit');?>
-<?}else{?>
-<div>Список пуст</div>
-<?}?>
-</form>
+    <form method="POST">
+        <button class="button" name="apply">Применить</button>
+        <? $this->displayPageControl('plEdit'); ?>
+        <? if ($rs->getCount()) { ?>
+            <table class="grid">
+                <tr>
+                    <th>Идентификатор</th>
+                    <th>Описание</th>
+                    <th>Ширина</th>
+                    <th>Высота</th>
+                    <th></th>
+                </tr>
+                <? while ($rs->next()) { ?>
+                    <tr>
+                        <td style="width:200px">
+                            <img src="/img/pic/opnbr_16.gif" title="<?= my_htmlspecialchars($rs->get("description")); ?>" alt="<?= my_htmlspecialchars($rs->get("description")); ?>"/> 
+                            <a href="?act=plEdit&id=<?= $rs->get('id'); ?>"><strong>[<?= $rs->get("id"); ?>]</strong></a>
+                        </td>
+                        <td>
+                            <small>[<?= my_htmlspecialchars($rs->get("description")); ?>]</small>
+                        </td>
+                        <td><input class="num" name="width[<?= $rs->get('id'); ?>]" value="<?= $rs->get('width') ?>"></td>
+                        <td><input class="num" name="height[<?= $rs->get('id'); ?>]" value="<?= $rs->get('height') ?>"></td>
+                        <td style="width:100px">
+                            <a href="?act=plEdit&id=<?= $rs->get('id'); ?>"><img src="/img/pic/edit_16.gif" title="Редактировать" alt="Редактировать"/></a>
+                            <a onclick="return confirm('Вы действительно хотите удалить запись?')" href="?act=plRemove&id=<?= $rs->get('id'); ?>"><img src="/img/pic/trash_16.gif" title="Удалить запись" border="0" alt=""/></a>
+                        </td>
+                    </tr>	
+                <? } ?>
+            </table>
+            <? $this->displayPageControl('plEdit'); ?>
+        <? } else { ?>
+            <div>Список пуст</div>
+        <? } ?>
+    </form>
 </div>
 <script type="text/javascript" src="/core/component/adv/adv_place.js"></script>
